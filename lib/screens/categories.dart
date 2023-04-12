8266ee601bdff26db2817b1818dce823aae4f1c0
Jadyn-Perez <jadyn@nautus.net>
2023-04-12 11:40:57 -0400

    initial commit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:mealsapp2/data/dummy_data.dart';
+import 'package:mealsapp2/screens/meals.dart';
+import 'package:mealsapp2/widgets/category_grid_item.dart';
+import 'package:mealsapp2/models/category.dart';
+import 'package:mealsapp2/models/meal.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+    required this.availableMeals,
+  });
+
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          ),
+      ],
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
